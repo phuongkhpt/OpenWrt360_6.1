95754c1fa869021ccf670a4faea4348a3c179c72
phuongkhpt <anhyeuemnhieu57@gmail.com>
2024-08-29 14:03:36 +0700

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -123,6 +123,8 @@ find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/
 # sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/model/cbi/v2ray_server/*.lua
 # sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/view/v2ray_server/*.htm
 rm -rf package/feeds/packages/fibocom-qmi-wwan
+rm -rf feeds/packages/kernel/fibocom-qmi-wwan
+rm -rf /packages/*/fibocom-qmi-wwan
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
